9182d860d0a8b723c7ab72fa3d22d2d5f1e53794
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2021-10-20 22:47:52 -0300

    Initial commit

---
+++ src/main/java/com/batista/fabio/testemc/TestemcApplication.java
@@ -0,0 +1,13 @@
+package com.batista.fabio.testemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestemcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestemcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batista/fabio/testemc/TestemcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batista.fabio.testemc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestemcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

fea84d5d4b42cf63adda4f0ee5cfc68d0f3de813
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2021-10-26 23:05:03 -0300

    Added cat Produtos & Categorias
    Added cat Produtos & Categorias

--- src/main/java/com/batista/fabio/testemc/TestemcApplication.java
+++ src/main/java/com/batista/fabio/testemc/TestemcApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.batista.fabio.testemc.domain.Categoria;
+import com.batista.fabio.testemc.domain.Produto;
 import com.batista.fabio.testemc.repositories.CategoriaRepository;
 
 @SpringBootApplication
@@ -22,8 +23,13 @@ public class TestemcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		
 		Categoria cat1 = new Categoria(1, "Informática");
 		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		Produto p1 = new Produto(1, "Computador", 2000.00);
+		Produto p2 = new Produto(2, "Impressora", 800.00);
+		Produto p3 = new Produto(3, "Mouse", 80.00);
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		
